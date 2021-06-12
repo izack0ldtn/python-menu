c928a179a2b81bd713127beb0fcd8bd1580dd01d
izack0ldtn <theosdudent@gmail.com>
2021-06-12 21:49:51 +0545

    Updated to Class level (OOP)

--- menu-for-sthV2.py
+++ menu-for-sthV2.py
@@ -1,65 +1,65 @@
-from rich import print
-# print('[black on white]Hello')
 import os
+from rich import print
 from msvcrt import getch
 
-list_of_items = ["Apple","Ball","Cat"]
-selected = 0
-enter_indices = []
 
-def consolePrint():
-    os.system('cls')
-    for x in range(len(list_of_items)):
-        print(entermanager(list_of_items[x]),textManager(list_of_items[x]))
+class Menu:
+    selected = 0
+    enter_indices = []
+    def __init__(self,array):
+        self.arr = array
 
-def upselect():
-    global selected
-    if selected <= 0:
-        selected = len(list_of_items)-1
-        consolePrint()
-    else:
-        selected -= 1
-        consolePrint()
+    def consoleDisplay(self):
+        os.system('cls')
+        for x in range(len(self.arr)):
+            print(self.entermanager(self.arr[x]),self.textManager(self.arr[x]))
 
-def downselect():
-    global selected
-    if selected >= len(list_of_items)-1:
-        selected = 0
-        consolePrint()
-    else:
-        selected += 1
-        consolePrint()
+    def textManager(self,text):
+        if self.selected == self.arr.index(text):
+            return f"[black on white]{text} "
+        else:
+            return f"[white]{text}"
 
-def entercase():
-    if selected not in enter_indices:
-        enter_indices.append(selected)
-    else:
-        enter_indices.remove(selected)
-    consolePrint()
+    def upselect(self):
+        if self.selected <= 0:
+            self.selected = len(self.arr)-1
+            self.consoleDisplay()
+        else:
+            self.selected -= 1
+            self.consoleDisplay()
 
-def entermanager(text):
-    for eachIndex in enter_indices:
-        if eachIndex == list_of_items.index(text):
-            return '[X]'
-    return '[ ]'
+    def downselect(self):
+        if self.selected >= len(self.arr)-1:
+            self.selected = 0
+            self.consoleDisplay()
+        else:
+            self.selected += 1
+            self.consoleDisplay()
 
-def textManager(text):
-    if selected == list_of_items.index(text):
-        return f"[black on white]{text} "
-    else:
-        return f"[white]{text}"
+    def entercase(self):
+        if self.selected not in self.enter_indices:
+            self.enter_indices.append(self.selected)
+        else:
+            self.enter_indices.remove(self.selected)
+        self.consoleDisplay()
 
-consolePrint()
+    def entermanager(self,text):
+        for eachIndex in self.enter_indices:
+            if eachIndex == self.arr.index(text):
+                return '[X]'
+        return '[ ]'
 
-while True:
-    key = ord(getch())
-    if key == 27: #ESC
-        break
-    elif key == 13: #Enter
-        entercase()
-    elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
-        key = ord(getch())
-        if key == 80: #Down arrow
-            downselect()
-        elif key == 72: #Up arrow
-            upselect()
+    def main(self):
+        self.consoleDisplay()
+        while True:
+            key = ord(getch())
+            if key == 27: #ESC
+                break
+            elif key == 13: #Enter
+                self.entercase()
+            elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
+                key = ord(getch())
+                if key == 80: #Down arrow
+                    self.downselect()
+                elif key == 72: #Up arrow
+                    self.upselect()
