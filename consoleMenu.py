60f81aa6a3620d256ef91ceb403e21cf52d209ef
izack0ldtn <theosdudent@gmail.com>
2021-06-13 17:27:35 +0545

    Added Classification Checkbox and radio button
    + Classes for check box and radio button
    + Parameter headertext(optional) and statement(compulsory).

--- consoleMenu.py
+++ consoleMenu.py
@@ -1,16 +1,21 @@
 import os
 from rich import print
+from rich.console import Console
 from msvcrt import getch
+console = Console()
 
-
-class Menu:
+class Menu_CheckBox:
     selected = 0
     enter_indices = []
-    def __init__(self,array):
+    def __init__(self,array,statement,headertext=None):
         self.arr = array
-
+        self.header = headertext
+        self.content = statement
     def consoleDisplay(self):
         os.system('cls')
+        if self.header != None:
+            console.print(self.header,justify="center")
+        console.print(self.content, style ="red")
         for x in range(len(self.arr)):
             print(self.entermanager(self.arr[x]),self.textManager(self.arr[x]))
 
@@ -70,3 +75,56 @@ class Menu:
         for x in self.enter_indices:
             holder.append(self.arr[x])
         return holder
+
+class Menu_RadioButton:
+
+    selected = 0
+
+    def __init__(self,array,headertext=None):
+        self.arr = array
+        self.header = headertext
+
+    def consoleDisplay(self):
+        os.system('cls')
+        if self.header != None:
+            print(self.header)
+        for eachItems in self.arr:
+            print(self.textManager(eachItems))
+
+    def upselect(self):
+        if self.selected <= 0:
+            self.selected = len(self.arr)-1
+        else:
+            self.selected -= 1
+        self.consoleDisplay()
+
+    def downselect(self):
+        if self.selected >= len(self.arr)-1:
+            self.selected = 0
+        else:
+            self.selected += 1
+        self.consoleDisplay()
+
+    def entercase(self):
+        return self.arr[self.selected]
+
+    def textManager(self,text):
+        if self.selected == self.arr.index(text):
+            return f"[black on white]{text} "
+        else:
+            return f"[white]{text}"    
+
+    def main(self):
+        self.consoleDisplay()
+        while True:
+            key = ord(getch())
+            if key == 27: #ESC
+                break
+            elif key == 13: #Enter
+                return self.entercase()
+            elif key == 224: #Special keys (arrows, f keys, ins, del, etc.)
+                key = ord(getch())
+                if key == 80: #Down arrow
+                    self.downselect()
+                elif key == 72: #Up arrow
+                    self.upselect()
--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-from consoleMenu import Menu
+from consoleMenu import Menu_CheckBox
 array = ['a','b','c','d','e']
-obj = menu(array)
+head = """Hello
+World"""
+stm = "Please Select One :"
+obj = Menu_CheckBox(array,stm,head)
 obj.main()
 print(obj.get_selected_items())
