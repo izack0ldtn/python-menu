cb36473f99acda71681b6c67d7cdf36dda234e0a
izack0ldtn <83655552+izack0ldtn@users.noreply.github.com>
2021-08-06 11:05:50 +0545

    delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from consoleMenu import Menu_CheckBox
-array = ['a','b','c','d','e']
-head = """Hello
-World"""
-stm = "Please Select One :"
-obj = Menu_CheckBox(array,stm,head)
-obj.main()
-print(obj.get_selected_items())
